96c4644500d5737a312933205a4a76e13eae3051
vismitmandlik <vismitmandlik03@gmail.com>
2023-07-25 22:50:19 +0530

    Day 3

---
+++ 03/script.js
@@ -0,0 +1,26 @@
+let num1 = 100
+let num2 = 40
+
+console.log(num1 + num2)
+
+//string
+const firstName = "Vismit"
+const lastName = "Mandlik"
+console.log(firstName + lastName)
+
+let isTrue = true
+let isNull = null
+
+// object
+const admin = {
+    firstName:"Vismit",
+    lastName:"Mandlik",
+    age:20,
+    islogged:true,
+}
+console.log(admin.firstName)
+
+console.log(typeof('68' * 1))
+//console.log('a' * 1) error:NaN
+
+
