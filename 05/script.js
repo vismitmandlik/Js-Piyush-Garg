6ebd0b64cede7923a7b2d064371bc08619d543e6
vismitmandlik <vismitmandlik03@gmail.com>
2023-07-30 21:32:48 +0530

    Day 4

---
+++ 05/script.js
@@ -0,0 +1,16 @@
+//Logical Operator
+
+const age = 20
+const gender = 'male'
+const number = 5
+if(age>=18 && gender=='male'){
+    console.log("You are adult male ")
+}
+
+if(age>=18 && gender=='female'){
+    console.log("You are adult male ")
+}
+
+if(number % 2 != 0){
+    console.log("Odd")
+}
